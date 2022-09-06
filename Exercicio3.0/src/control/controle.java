f7203f9bba1a3498107c81222b236e6382df514c
GilmardaSilva1 <101288726+GilmardaSilva1@users.noreply.github.com>
2022-09-06 16:15:00 -0300

    Add files via upload

---
+++ Exercicio3.0/src/control/controle.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import java.sql.SQLException;
+import model.model;
+import modelDAO.modelDAO;
+
+/**
+ *
+ * @author aluno
+ */
+public class controle {
+    
+    public void cadastrarJogo(int Codigo,String Nome,String Categoria, Double preco) throws SQLException{
+        System.out.println(Nome);
+        System.out.println(Codigo);
+        System.out.println(Categoria);
+        System.out.println(preco);
+        
+        model m1 = new model(Codigo,Nome,Categoria,preco);
+        
+        modelDAO md = new modelDAO();
+        md.inserirJogo(m1);
+    }
+    
+    public void excluirJogo(int Codigo) throws SQLException {
+        modelDAO md = new modelDAO();       
+        md.excluirJogo(Codigo);
+        System.out.println("LLLLL");
+    }
+    
+}
